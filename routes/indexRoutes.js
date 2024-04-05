871c6223846746cc789208e6f35ff6362545fb39
Celso Modesto Jr <cnmodesto@gmail.com>
2024-04-04 21:39:01 -0300

    Adjusts on indexRoutes.js and users/userForm.hbs

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('/albums')
+    res.redirect('/collections')
 });
 
 module.exports = router;
